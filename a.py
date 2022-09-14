76d09fe5a0af27ab3b00db7539d114f0502763a1
Bryce <46682358+bbible3@users.noreply.github.com>
2022-09-14 18:11:56 -0400

    Got the proper settings in memory, need to output

--- a.py
+++ a.py
@@ -4,3 +4,9 @@ from configs import ConfigFile
 myConfig = ConfigFile("./memhier/trace.config")
 myConfig.load_file()
 myConfig.parse_lines()
+
+#Calculate the number of index and offset bits for the different portions of the memory hierarchy
+myConfig.process_config()
+
+print("-----------------")
+myConfig.output_config()
--- configs.py
+++ configs.py
@@ -1,3 +1,4 @@
+import math
 import re
 
 ##This file manages loading config files for the application
@@ -23,7 +24,7 @@ class Config:
 ## It also handles reading and processing of config files
 class ConfigFile:
     def __init__(self, filename):
-        self.configs = []
+        self.configs = {}
         self.filename = filename
         self.virtual_addresses = False
         self.tlb = False
@@ -31,8 +32,25 @@ class ConfigFile:
         self.lines = []
 
 
+        self.tlb_bits = -1
+
+        self.page_table_index_bits = -1
+        self.page_table_offset_bits = -1
+
+        self.data_cache_index_bits = -1
+        self.data_cache_offset_bits = -1
+
+        self.l2_cache_index_bits = -1
+        self.l2_cache_offset_bits = -1
+
+        self.cache_size = -1
+        self.block_offset_len = -1
+
+        self.address_len = -1
+        self.virtual_address_len = -1
+
     def add_config(self, config):
-        self.configs.append(config)
+        self.configs[config.name] = config
 
     def load_file(self):
         with open(self.filename) as f:
@@ -65,6 +83,7 @@ class ConfigFile:
                         line_split = line.split(special_line)[1]
                         val = True if 'y' in line_split else False
                         self.l2_cache = val
+                    cur_working_config.name = "Root"
                     print("Updated file-wide config: " + special_line + " to " + str(val))
                     break
 
@@ -122,4 +141,69 @@ class ConfigFile:
                             print("Current config name: " + cur_working_config.name)
                             print("New config name: " + config_type)
                         print("Setting working config type to: " + config_type)
-               
+    def process_config(self):
+        #This function calculates the number of index and offset bits for the different portions of the memory hierarchy
+        print("Calculating bits for configs")
+        
+        #Cache "line" and "block" are interchangable.
+        #In the in class example, 
+        #How many sets in 8MB cache? "How many blocks can this cache hold?"
+        #cache size / block size = number of frames
+
+        print("I am file: " + self.filename)
+        print("len of self.configs: " + str(len(self.configs)))
+        for config in self.configs:
+            #Print the config name
+            print("Config name: " + config)
+            #Get the config object
+            config_obj = self.configs[config]
+
+            if config == "Data TLB ":
+                #How many bits to index the TLB?
+                #This is log2(num_sets * set_size)
+                self.tlb_bits = int(math.log2(config_obj.num_sets * config_obj.set_size))
+                print("TLB bits: " + str(self.tlb_bits))
+
+            if config == "Page Table ":
+                num_virtual_pages = config_obj.num_vpages
+                num_physical_pages = config_obj.num_ppages
+                page_size = config_obj.page_size
+                #How many bits to index the page table?
+                self.page_table_index_bits = int(math.log2(num_virtual_pages))
+                print("Page table index bits: " + str(self.page_table_index_bits))
+                #How many bits for page offset?
+                self.page_table_offset_bits = int(math.log2(page_size))
+                print("Page table offset bits: " + str(self.page_table_offset_bits))
+
+            if config == "Data Cache ":
+                num_sets = config_obj.num_sets
+                set_size = config_obj.set_size
+                line_size = config_obj.line_size
+                #How many bits to index the cache?
+                self.cache_index_bits = int(math.log2(num_sets))
+                print("Cache index bits: " + str(self.cache_index_bits))
+                #How many bits for cache offset?
+                self.cache_offset_bits = int(math.log2(line_size))
+                print("Cache offset bits: " + str(self.cache_offset_bits))
+
+            if config == "L2 Cache ":
+                num_sets = config_obj.num_sets
+                set_size = config_obj.set_size
+                line_size = config_obj.line_size
+                #How many bits to index the cache?
+                self.l2_cache_index_bits = int(math.log2(num_sets))
+                print("L2 Cache index bits: " + str(self.l2_cache_index_bits))
+                #How many bits for cache offset?
+                self.l2_cache_offset_bits = int(math.log2(line_size))
+                print("L2 Cache offset bits: " + str(self.l2_cache_offset_bits))
+
+        #Calculate my virtual address length
+        self.virtual_address_length = self.page_table_index_bits + self.page_table_offset_bits
+        print("Virtual address length: " + str(self.virtual_address_length))
+
+    def output_config(self):
+        #This function produces output in the same way that the in class example did
+        print(f"Data TLB Contains {self.configs['Data TLB '].num_sets} sets.")
+        print(f"Each set contains {self.configs['Data TLB '].set_size} entries.")
+            
+
